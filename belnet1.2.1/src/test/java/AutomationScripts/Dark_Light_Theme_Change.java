9b13070c5ece461ea6db52e17db6bf98a2003b8e
befor <befor@DESKTOP-SGRJ71S>
2023-07-25 15:01:44 +0530

    Dark And Light Mode Theme Change

--- belnet1.2.1/src/test/java/AutomationScripts/Dark_Light_Theme_Change.java
+++ belnet1.2.1/src/test/java/AutomationScripts/Dark_Light_Theme_Change.java
@@ -8,25 +8,15 @@ import io.appium.java_client.AppiumBy;
 
 public class Dark_Light_Theme_Change extends DesiredCapabilitiesOfDevice{
 	@Test
-	public void scrol() throws InterruptedException {
+	public void DarkandLightThemeChagne() throws InterruptedException {
+		 Thread.sleep(3000);
 		WebElement se=driver.findElement(By.xpath("//android.widget.Button[@content-desc=\"CLOSE\"]"));
 	       se.click();	
  	   Thread.sleep(3000);
-	WebElement e3=driver.findElement(By.xpath("//android.widget.ImageView[@index=\"8\"]"));
-    e3.click();
-    Thread.sleep(5000);
-    WebElement e7=driver.findElement(AppiumBy.accessibilityId("Contributor exit node\n" +
-            "17 Nodes"));
-    e7.click();
-    Thread.sleep(2000);
-    String scrollElement = "new UiScrollable(new UiSelector().scrollable(true).instance(0)).scrollForward(3)"; //.scrollIntoView(new UiSelector().description(\"6o5ushgtbuaowsmt614iy1ibtzia7mt87ngqubdkwforid1i6pro.bdx\n" +
-            //"Canada\")";
-    Thread.sleep(5000);
-   WebElement seven0=driver.findElement(new AppiumBy.ByAndroidUIAutomator(scrollElement));
-   Thread.sleep(5000);
-   String scrollElement2 = "new UiScrollable(new UiSelector().scrollable(true).instance(0)).scrollForward()"; //.scrollIntoView(new UiSelector().description(\"6o5ushgtbuaowsmt614iy1ibtzia7mt87ngqubdkwforid1i6pro.bdx\n" +
-   //"Canada\")";
-Thread.sleep(5000);
-WebElement seven10=driver.findElement(new AppiumBy.ByAndroidUIAutomator(scrollElement2));
-Thread.sleep(5000);
-}}
+	for(int i=0;i<=9;i++) {
+		WebElement seven10=driver.findElement(By.xpath("//android.widget.ImageView[@index='2']")); // Click on Light Theme button
+	    seven10.click();
+	    WebElement seven311=driver.findElement(By.xpath("//android.widget.ImageView[@index='2']")); // Click on Dark Theme button
+	    seven311.click();
+}
+	}}
